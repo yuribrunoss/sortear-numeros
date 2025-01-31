453cbac3d53a93e7372930cc9a5f5db7b153e3d3
Yuri Bruno <196816670+yuribrunoss@users.noreply.github.com>
2025-01-31 09:35:20 -0300

    Projeto Inicial

---
+++ app.js
@@ -0,0 +1,12 @@
+function sortear(){
+    
+   let  quantidade  = document.getElementById('qtd').value;
+   let  doNumero    = document.getElementById('de').value;
+   let  ateNumero   = document.getElementById('ate').value;
+
+   alert(`Quantidade:   ${quantidade}`);
+   alert(`Do número:    ${doNumero}`);
+   alert(`Até o número: ${ateNumero}`);
+   
+
+}

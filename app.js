20da40515efa786027fbf92eb81114b452b38c4f
Yuri Bruno <196816670+yuribrunoss@users.noreply.github.com>
2025-02-03 15:48:25 -0300

    Ativando o botão reiniciar

--- app.js
+++ app.js
@@ -1,12 +1,63 @@
 function sortear(){
     
-   let  quantidade  = document.getElementById('qtd').value;
-   let  doNumero    = document.getElementById('de').value;
-   let  ateNumero   = document.getElementById('ate').value;
+   let  quantidade  = parseInt(document.getElementById('qtd').value);
+   let  doNumero    = parseInt(document.getElementById('de').value);
+   let  ateNumero   = parseInt(document.getElementById('ate').value);
 
-   alert(`Quantidade:   ${quantidade}`);
-   alert(`Do número:    ${doNumero}`);
-   alert(`Até o número: ${ateNumero}`);
+   let sorteados = [];
+   let numero;
+
+   for (let i = 0; i < quantidade; i++){
+      
+      numero = obterNumeroAleatorio(doNumero, ateNumero);
+      
+      while (sorteados.includes(numero)) {
+
+         numero = obterNumeroAleatorio(doNumero, ateNumero);
+
+      }
+      
+      sorteados.push(numero);
+   }
+
+   let resultado = document.getElementById('resultado');
+   resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+
+   alterarStatusBotaoReiniciar();
+
+}
+
+function obterNumeroAleatorio(min, max){
+   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotaoReiniciar() {
    
+   let botaoReiniciar = document.getElementById('btn-reiniciar');
+   
+   if (botaoReiniciar.classList.contains('container__botao-desabilitado')) {
+
+      botaoReiniciar.classList.remove('container__botao-desabilitado');
+      botaoReiniciar.classList.add('container__botao');
+      
+   } else {
+
+      botaoReiniciar.classList.remove('container__botao');
+      botaoReiniciar.classList.add('container__botao-desabilitado');
+
+   }
+      
+   
+}
+
+function reiniciar() {
+
+   document.getElementById('qtd').value = '';   
+   document.getElementById('de').value = '';   
+   document.getElementById('ate').value = '';   
+   
+   document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+
+   alterarStatusBotaoReiniciar();
 
 }
